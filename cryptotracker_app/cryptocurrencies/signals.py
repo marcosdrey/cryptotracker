8dfa557e404e31a1ef9714aed76aa367d1394a0f
marcosdrey <programmarcos05@gmail.com>
2025-01-13 12:06:56 -0300

    Fix alert filtering and data sent to the webhook

--- cryptotracker_app/cryptocurrencies/signals.py
+++ cryptotracker_app/cryptocurrencies/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
-from django.db.models import Q
+from django.db.models import Q, FloatField
+from django.db.models.functions import Cast
+from django.utils.timezone import localtime
 from django.dispatch import receiver
-from django.core import serializers
 from .models import CryptocurrencyPrice, CryptocurrencyAlert
 from services.notify_alert_service import NotifyAlertService
 
@@ -11,17 +12,21 @@ def send_cryptos_price_webhook(sender, instance, created, **kwargs):
     try:
         if created:
             alerts = CryptocurrencyAlert.objects.filter(
-                Q(alert_type='price_above', value__gt=instance.price),
-                Q(alert_type='price_below', value__lt=instance.price),
-                cryptocurrency=instance.cryptocurrency,
+                Q(cryptocurrency=instance.cryptocurrency),
+                Q(alert_type='price_above', value__lt=instance.price) |
+                Q(alert_type='price_below', value__gt=instance.price)
+            ).annotate(
+                alert_value=Cast('value', output_field=FloatField())
+            ).values(
+                'user__username', 'user__email', 'alert_type', 'alert_value'
             )
             notify_alert_service = NotifyAlertService()
             data = {
                 'event_type': 'create_cryptocurrency_price',
                 'cryptocurrency': instance.cryptocurrency.name,
-                'price': float(instance.price),
-                'timestamp': instance.timestamp.strftime('%Y-%m-%d, %H:%M:%S'),
-                'alerts': serializers.serialize('json', alerts),
+                'current_price': float(instance.price),
+                'timestamp': localtime(instance.timestamp).strftime('%Y-%m-%d, %H:%M:%S'),
+                'alerts': list(alerts),
             }
             notify_alert_service.send_alert(data)
     except:
